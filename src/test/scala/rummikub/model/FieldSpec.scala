8ceb54b860f15e415faafad2f69cc1577866b387
Andreas Loeffler <andreas.loeffler00@gmail.com>
2020-11-06 08:22:38 +0100

    More RowSpec and PlayerSpec tests added

--- src/test/scala/rummikub/model/FieldSpec.scala
+++ src/test/scala/rummikub/model/FieldSpec.scala
@@ -16,17 +16,23 @@ class FieldSpec extends AnyWordSpec with Matchers {
       "be empty" in {
         field.isEmpty should be(true)
       }
+      "when unapplied" in{
+        Field.unapply(field).get should be(null)
+      }
     }
     "set to a specific string " should {
       val nonEmptyField = Field("Yellow5")
       "return that tile" in {
-        nonEmptyField.tile startsWith ("Yellow5")
+        nonEmptyField.tile startsWith "Yellow5"
       }
       "have a name " in {
-        nonEmptyField.toString startsWith ("Yellow5")
+        nonEmptyField.toString startsWith "Yellow"
       }
       "not be empty" in {
         nonEmptyField.isEmpty should be(false)
+      }
+      "when applied" in{
+        Field.apply("Yellow5").toString should be("Yellow5")
       }
     }
   }
--- src/test/scala/rummikub/model/PlayerSpec.scala
+++ src/test/scala/rummikub/model/PlayerSpec.scala
@@ -4,12 +4,18 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class PlayerSpec extends AnyWordSpec with Matchers {
-  "A player" should {
-    "have a name" in {
-      Player("testName").name startsWith "testName"
-    }
-    "have a representation with a String" in {
-      Player("testName").toString startsWith "testName"
+  "A player" when {
+    "new " should {
+      val player = Player("testName")
+      "have a name " in {
+        player.name should be ("testName")
+      }
+      "have a representation with a String" in {
+        player.toString should be ("testName")
+      }
+      "when unapplied" in{
+        Player.unapply(player).get should be("testName")
+      }
     }
   }
 }
