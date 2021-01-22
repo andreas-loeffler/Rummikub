38afa3eb50bd8b06a959f2c0c71ac8adcb3a1d34
Andreas Loeffler <andreas.loeffler00@gmail.com>
2021-01-22 08:04:01 +0100

    added UI_Type to Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM hseeberger/scala-sbt:8u265_1.4.3_2.13.4
+ENV UI_TYPE=tui
 WORKDIR /rummikub
 ADD . /rummikub
-CMD sbt test
+CMD sbt run
--- src/main/scala/rummikub/Rummikub.scala
+++ src/main/scala/rummikub/Rummikub.scala
@@ -8,10 +8,14 @@ import controller.controllerComponents.ControllerInterface
 import scala.io.StdIn.readLine
 
 object Rummikub {
+  var UI_Type: Boolean = if (System.getenv("UI_TYPE").equals("tui")) true else false
+
   val injector = Guice.createInjector(new RummikubModule)
   val controller = injector.getInstance(classOf[ControllerInterface])
   val textUI = new TextUI(controller)
-  val gui = new SwingGui(controller)
+  if (!UI_Type) {
+    val gui = new SwingGui(controller)
+  }
   controller.smallGB
 
   def main(args: Array[String]): Unit = {
@@ -26,4 +30,5 @@ object Rummikub {
   }
 
 }
+
 //test
