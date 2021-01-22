d69ace1800392583a63e945c874a4700c1362c33
Andreas Loeffler <andreas.loeffler00@gmail.com>
2021-01-22 08:32:33 +0100

    updated UI_TYPE

--- src/main/scala/rummikub/Rummikub.scala
+++ src/main/scala/rummikub/Rummikub.scala
@@ -8,16 +8,17 @@ import controller.controllerComponents.ControllerInterface
 import scala.io.StdIn.readLine
 
 object Rummikub {
-  //var UI_Type: Boolean = if (System.getenv("UI_TYPE").equals("tui")) true else false
+  var UI_Type: Boolean = if (System.getenv("UI_TYPE").equals("tui")) true else false
 
   val injector = Guice.createInjector(new RummikubModule)
   val controller = injector.getInstance(classOf[ControllerInterface])
   val textUI = new TextUI(controller)
   controller.smallGB
-  val gui = new SwingGui(controller)
 
   def main(args: Array[String]): Unit = {
-
+    if (!UI_Type) {
+      val gui = new SwingGui(controller)
+    }
     var input: String = ""
     do {
       print(">>")
